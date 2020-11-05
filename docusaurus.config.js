40f76024412a710c9838c38b3bf0019af459edf8
Yuval Hazaz <hazaz.yuval@gmail.com>
2020-11-05 19:34:19 +0200

    edit urk

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,13 +71,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/amplication/docs/edit/main/docs-website/',
+            'https://github.com/amplication/docs/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/amplication/docs/edit/main/docs-website/blog/',
+            'https://github.com/amplication/docs/edit/main/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
