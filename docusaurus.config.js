7ad8f45167333bd269457ed2e4e73604e5ca1dcc
Yuval Hazaz <hazaz.yuval@gmail.com>
2021-01-16 21:23:00 +0200

    redirect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,4 +85,17 @@ module.exports = {
       },
     ],
   ],
+  plugins: [ 
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            to: '/docs', // string
+            from: ['/guides/getting-started'], 
+          },
+        ],
+      },
+    ],
+  ],
 };
