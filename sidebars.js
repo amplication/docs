7380140fc0536cef1e37c3aca517ad07d855a810
Yuval Hazaz <hazaz.yuval@gmail.com>
2021-03-01 16:14:01 +0200

    update menu

--- sidebars.js
+++ sidebars.js
@@ -4,19 +4,19 @@ module.exports = {
       'welcome',
       'getting-started/generated-app', 
       'getting-started/first-app', 
-      'how-to/run-dev-env',
       'getting-started/sync-with-github', 
+      'how-to/run-dev-env',
+      'how-to/add-custom-code',
       'getting-started/deploy', 
       'getting-started/generated-app-api',
       'getting-started/connect-server-to-github',
-      'getting-started/relations'
     ],
     'How To':[
       'how-to/how-to-create-app',
       'how-to/how-to-create-entity',
       'how-to/how-to-create-entity-field',
       'how-to/how-to-commit-changes',
-      'how-to/add-custom-code',
+      'getting-started/relations'
     ],
     'Customize Your Application':[
       'custom-code/add-business-logic',
