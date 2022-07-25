e19c2830864e7af0e4d931dfd2ff35843022e512
Moshe Forman <96979533+MoFoGo@users.noreply.github.com>
2022-07-25 14:29:07 +0300

    new-faq

--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,9 @@
 module.exports = {
   someSidebar: {
+
+    "Frequently Asked Questions": [
+      "faqs/faqs",      
+    ],
     "Getting Started": [
       "welcome",
       "getting-started/generated-app",
