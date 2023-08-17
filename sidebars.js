891721fbcd7d8dbddc0de6c9b29b66ab83d88fdb
Derick Sozo <derick.r.ruiz@gmail.com>
2023-08-17 23:08:43 +0700

    (docs): Adding a new How To page regarding the SSO feature for Enterprise users.

--- sidebars.js
+++ sidebars.js
@@ -101,6 +101,7 @@ const sidebars = {
         "getting-started/how-prisma-schema-is-converted-into-entities",
         "how-to/erd-view-for-amplication-entities",
         "getting-started/relations",
+        "how-to/enterprise-sso",
         "how-to/how-to-set-access-permissions",
         "how-to/how-to-commit-changes",
         "how-to/change-base-branch-for-pull-requests",
