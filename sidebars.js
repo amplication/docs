e2718382357efbe695c1df0ffebecd5504bbc603
Derick Sozo <derick.r.ruiz@gmail.com>
2023-08-21 19:14:39 +0700

    (docs): Removing a duplicate of Entities Relations that appears twice in the sidebar.

--- sidebars.js
+++ sidebars.js
@@ -104,7 +104,6 @@ const sidebars = {
         "how-to/how-to-set-access-permissions",
         "how-to/how-to-commit-changes",
         "how-to/change-base-branch-for-pull-requests",
-        "getting-started/relations",
         "how-to/base-directories",
         "how-to/api-admin-ui-settings",
         "how-to/create-message-broker",
